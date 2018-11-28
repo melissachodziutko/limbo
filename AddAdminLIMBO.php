eab71b3e65878b1d3ef01c9b3172d218fec78420
Amy-V-M <45437812+Amy-V-M@users.noreply.github.com>
2018-11-28 16:05:15 -0500

    Add files via upload

---
+++ AddAdminLIMBO.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Add New Admin </title>
+</head>
+<body>
+	<button> <a href="LandingPageLIMBO.php"> Back to landing page </a></button>
+	<button> <a href="losttable.php"> Lost something </a> </button>
+	<button> <a href="foundtable.php"> Found something </a> </button>
+	<button> <a href="adminlogin.php"> Admins </a> </button>
+	<!-- Put in table of admin information -->
+	<?php
+	
+	?>
+	<h1> Please complete and submit the following form to add a new admin: </h1>
+	<form action='#'>
+		<h3> (required fields are marked with an *) </h3>
+		<p> 
+		<label> Admin's Username: * </label>
+			<input type="text" required> </input>
+		</p>
+		<p>
+		<label> Admin's Email: * </label>
+			<input type="email" required> </input>
+		</p>
+		<p>
+		<label> Admin's Password: * </label>
+			<input type="text" required> </input>
+		</p>
+		<p>
+		<label> Your Password: * </label>
+			<input type="text" required> </input>
+		</p>
+		<p> 
+			<input type="submit" value="Add"> </input>
+			<input type="reset" value="Clear"> </input>
+		</p>
+</body>
+</html>
