9eba8a45c2b1ef39d0f9243b400bb35226c15025
melissachodziutko <11694558+melissachodziutko@users.noreply.github.com>
2018-11-28 16:16:46 -0500

    Update helpers_limbo.php

--- includes/helpers_limbo.php
+++ includes/helpers_limbo.php
@@ -41,6 +41,43 @@ function show_lost_records($dbc) {
 		  mysqli_free_result( $results ) ;
     }
 }
+function show_lost_ql1_records($dbc) {
+	# Create a query to get the description, create_date, item_status
+    
+	$query = "SELECT description, create_date, item_status FROM stuff WHERE item_status= 'lost' AND update_date < '2017-12-06 07:12:46'  ORDER BY create_date DESC" ;
+	
+	# Execute the query
+	$results = mysqli_query( $dbc , $query ) ;
+	check_results($results) ;
+
+	# Show results
+	if( $results )
+	{
+  		# But...wait until we know the query succeed before
+  		# rendering the table start.
+  		 echo '<H1>Lost Items</H1>' ;
+		echo '<TABLE>';
+		  echo '<table border = "1"';
+		  echo '<TR>';
+		  echo '<TH>Description</TH>';
+		  echo '<TH>Creation Date</TH>';
+		  echo '<TH>Status</TH>';
+		  echo '</TR>';
+		  # For each row result, generate a table row
+		  while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+		  {
+			echo '<TR>' ;
+			echo '<TD>' . $row['description'] . '</TD>' ;
+			echo '<TD>' . $row['create_date'] . '</TD>' ;
+			echo '<TD>' . $row['item_status'] . '</TD>' ;
+			echo '</TR>' ;
+		  }
+		  # End the table
+		  echo '</TABLE>';
+		  # Free up the results in memory
+		  mysqli_free_result( $results ) ;
+    }
+}
 function show_found_records($dbc) {
 	# Create a query to get the description, create_date, item_status
     
