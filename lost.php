011339ab48873ffe5dba1556068809c7beb5a73f
melissachodziutko <11694558+melissachodziutko@users.noreply.github.com>
2018-11-28 17:31:52 -0500

    Update lost.php

--- lost.php
+++ lost.php
@@ -62,25 +62,102 @@ mysqli_close( $dbc ) ;
 ?>
 
 <!-- HTML with embedded PHP --> 
-<form action="lost.php" method="POST"> 
+<form action="found.php" method="POST"> 
+
 <p>Location: 
+
     <select id="id" name="id">                      
-  <option value="0">--Select Animal--</option>
-  <option value="1">Cat</option>
-  <option value="2">Dog</option>
-  <option value="3">Cow</option>
+
+  <option value="0">--Select Location--</option>
+
+    <option value="1">Byrne House</option>
+
+    <option value="2">Cannavino Library</option>
+
+    <option value="3">Champagnat</option>
+
+    <option value="4">Chapel</option>
+
+    <option value="5">Cornell Boathouse</option>
+
+    <option value="6">Donnelly</option>
+
+    <option value="7">Dyson Center</option>
+
+    <option value="8">Fern Tor</option>
+
+    <option value="9">Fontaine Hall</option>
+
+    <option value="10">Foy Townhouses</option>
+
+    <option value="11">Fulton Street Townhouses (Lower)</option>
+
+    <option value="12">Fulton Street Townhouses (Upper)</option>
+
+    <option value="13">Greystone Hall</option>
+
+    <option value="14">Hancock Center</option>
+
+    <option value="15">Kieran Gatehouse</option>
+
+    <option value="16">Kirk House</option>
+
+    <option value="17">Leo Hall</option>
+
+    <option value="18">Longview Park</option>
+
+    <option value="19">Lowell Thomas</option>
+
+    <option value="20">Marian Hall</option>
+
+    <option value="21">McCann Center</option>
+
+    <option value="22">Mid-Rise Hall</option>
+
+    <option value="23">North Campuse Housing Complex</option>
+
+    <option value="24">St. Anne's Hermitage</option>
+
+    <option value="25">St. Peter's</option>
+
+    <option value="26">Science and Allied Health Building</option>
+
+    <option value="27">Sheahan Hall</option>
+
+    <option value="28">Steel Plant Studios and Gallery</option>
+
+    <option value="29">Student Center/Music Building</option>
+
+    <option value="30">West Cedar Townhouses (Lower)</option>
+
+    <option value="31">West Cedar Townhouses (Upper)</option>
+
+</select>
+
 </select>
 
 
+
+
+
     <!--<input type="text" name="id" value="<?php 
+
     if (isset($_POST['id'])) echo $_POST['id']; ?>">--> </p> 
+
 <p>Description: <input type="text" name="description" value="<?php 
+
     if (isset($_POST['description'])) echo $_POST['description']; ?>"></p> 
+
 <p>Room: <input type="text" name="room" value="<?php 
+
     if (isset($_POST['room'])) echo $_POST['room']; ?>"></p>
+
 <p>Your name: <input type="text" name="finder" value="<?php 
+
     if (isset($_POST['finder'])) echo $_POST['finder']; ?>"></p>
+
 <p><input type="submit"></p> 
+
 </form>
 
 	
