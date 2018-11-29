447718491e126bfb6e96d57be98cba00ef926717
melissachodziutko <11694558+melissachodziutko@users.noreply.github.com>
2018-11-29 10:06:05 -0500

    Add files via upload

--- LandingPageLIMBO.php
+++ LandingPageLIMBO.php
@@ -1,43 +1,104 @@
 <!DOCTYPE html>
 <html>
     
-    <head>
-        
+    <head><style>
+body, html {
+    height: 100%;
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.hero-image {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("marist.jpg");
+  height: 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 40px;
+}
+
+.hero-text button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 10px 25px;
+  color: black;
+  background-color: #ddd;
+  text-align: center;
+  cursor: pointer;
+}
+
+.hero-text button:hover {
+  background-color: #555;
+  color: white;
+}
+
+input[type=submit] {
+    border: 1px solid #f44c0e;
+    color: #fff;
+    background: tomato;
+    padding: 10px 20px;
+    border-radius: 5px;
+}
+input[type=submit]:hover {
+    background: #f44c0e;
+}
+</style>
+         <link rel="stylesheet" href="overall.css">
+        <div class="hero-image">
+  <div class="hero-text">
+    <h1>LIMBO</h1>
+    <p>Marist's Number 1 Lost and Found Database</p>
+  </div>
+</div>
         <title>Welcome to Limbo!</title>
         <meta name="description" content="An interactive getting started guide for Brackets.">
-        <link rel="stylesheet" href="overall.css">
+        <!--<link rel="stylesheet" href="overall.css">-->
     </head>
     <body>
         <!-- this would lead to adminlogin.html -->
-		<form action="adminlogin.php" align = "left">
+        
+		<form action="adminlogin.php" >
         <input type="submit" value="Admins" />
         </form>
-        <h1>Welcome to Limbo!</h1>
-     
-        <h2> Marist's Lost and Found Database </h2>
-        
-        
+        <center>
         <p>
            With Limbo, you can report items that you've lost or found on the Marist College campus. 
         </p>
-		
+        </center>
         
+            <div class = "all-buttons">
+           <center>     
 		<!-- lead to lost.html -->
-        <form action="losttable.php">
+        <form action="losttable.php" >
         <input type="submit" value="Lost Something?" />
         </form>
-
+        
+            
+           
 		<!-- lead to found.html -->
-        <form action="foundtable.php">
+        <form action="foundtable.php" >
         <input type="submit" value="Found Something?" />
         </form>
-
-        <form action="QuickLinksLIMBO.php">
+                
+            
+           
+        <form action="QuickLinksLIMBO.php" align = "top">
         <input type="submit" value="Quick Links" />
         </form>
+               </center>
+            </div>
 		
-		
-       
         </body>
         
         
--- QuickLinksLIMBO.php
+++ QuickLinksLIMBO.php
@@ -1,17 +1,88 @@
 <!DOCTYPE html>
 <html>
-     <head>
+    
+    <head><style>
+body, html {
+    height: 100%;
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.hero-image {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("marist.jpg");
+  height: 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 40px;
+}
+
+.hero-text button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 10px 25px;
+  color: black;
+  background-color: #ddd;
+  text-align: center;
+  cursor: pointer;
+}
+
+.hero-text button:hover {
+  background-color: #555;
+  color: white;
+}
+
+input[type=submit] {
+    border: 1px solid #f44c0e;
+    color: #fff;
+    background: tomato;
+    padding: 10px 20px;
+    border-radius: 5px;
+}
+input[type=submit]:hover {
+    background: #f44c0e;
+}
+</style>
 		<title> Quick Links </title>
-		<link rel="stylesheet" href="QuickLinksCSS.css">
+		<link rel="stylesheet" href="overall.css">
      </head>
      <body>
           <!--lead back to PracticePageLIMBO.html-->
 		<form action="LandingPageLIMBO.php" align = "left">
         <input type="submit" value="Go to Landing Page" />
         </form>
-		<button><a href="lost.php"> Lost something</a> </button>
-		<button> <a href="found.php">Found something</a> </button>
-		<button><a href="adminlogin.php">Admin Login</a></button>
+		 <div class = "all-buttons">
+           <center>     
+		<!-- lead to lost.html -->
+        <form action="losttable.php" >
+        <input type="submit" value="Lost Something?" />
+        </form>
+        
+            
+           
+		<!-- lead to found.html -->
+        <form action="foundtable.php" >
+        <input type="submit" value="Found Something?" />
+        </form>
+                
+            
+           
+        <form action="adminlogin.php" align = "top">
+        <input type="submit" value="Admin" />
+        </form>
+               </center>
+            </div>
 		<h1> Click on an item for more details! </h1>
 		<p> 
 			Reported in last
@@ -48,8 +119,9 @@
 </form>
 		</p>
 		<div class="bothForms">
+            <center>
 			<div class="rLost">
-				<h2> Recently Lost </h2>
+				
 				<!-- Put recently lost table here -->
                 <?php
                  #$time = $_POST['id'];
@@ -62,7 +134,7 @@
                 ?>
 			</div>
 			<div class="rFound">
-				<h2> Recently Found </h2>
+				
 				<!-- Put recently found table here -->
                 <?php
                 #$time = $_POST['id'];
@@ -74,6 +146,9 @@
                 show_found_records($dbc);
                 ?>
 			</div>
+                <br>
+                <br>
+                </center>
 		</div>
 	 
      </body>
